e71134e01c54fd84d4c3c6913d7f7a77f243066e
Josemaria <josemariaodumodu@gmail.com>
2020-06-12 15:28:29 +0100

    add multiple questions
    add submit button
    make submit button clickable only when questions finished

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
+const nextButton = document.querySelector('#next-button');
+const submitButton = document.querySelector('#submit-button');
 const optionForm = document.querySelector('#option-form');
 const questionContentP = document.querySelector('#question-content');
 const optionInputs = document.querySelectorAll('.option input');
 const optionContentSpans = document.querySelectorAll('.option-content');
-const qOption = {
-    question: 'Jose Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
-    options: [
-        '1Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-        '2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-        '3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-        '4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-    ],
-};
+let currentQuestion = 0;
+const answeredOptions = [];
+const qOptions = [
+    {
+        question: '2Jose Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+        options: [
+            '1Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+            '2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+            '3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+            '4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        ],
+    },
+    {
+        question: '3Jose Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+        options: [
+            '1Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+            '2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+            '3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+            '4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        ],
+    },
+    {
+        question: '4Jose Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+        options: [
+            '1Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+            '2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+            '3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+            '4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        ],
+    },
+];
 
-const changeQuestions = (qOption) => {
+const changeDOMQuestion = (qOption) => {
     questionContentP.textContent = qOption.question;
     optionContentSpans.forEach((span, index) => {
         span.textContent = qOption.options[index];
@@ -24,9 +48,22 @@ const changeQuestions = (qOption) => {
     });
 };
 
+const changeQuestion = () => {    
+    if (currentQuestion === (qOptions.length - 2)) {
+        nextButton.disabled = true;
+        submitButton.disabled = false;
+    }
+    ++currentQuestion;
+    changeDOMQuestion(qOptions[currentQuestion]);
+};
+
 optionForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    let options = Object.fromEntries(new FormData(optionForm));
-    console.log(options);
-    changeQuestions(qOption);
+    let answeredOption = Object.fromEntries(new FormData(optionForm));
+    answeredOptions.push(answeredOption.option);
+    if (currentQuestion === (qOptions.length - 1)) {
+        alert(`your answers are ${answeredOptions}`);
+    } else {
+        changeQuestion();
+    }
 });
