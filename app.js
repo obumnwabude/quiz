6290cfefc7bea1252b61fc8b78fedf2afeafdc78
Josemaria <josemariaodumodu@gmail.com>
2020-06-12 14:16:01 +0100

    make questions to change on next button

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const optionForm = document.querySelector("#option-form");
+const optionForm = document.querySelector('#option-form');
+const questionContentP = document.querySelector('#question-content');
+const optionContentSpans = document.querySelectorAll('.option-content');
+const qOption = {
+    question: 'Jose Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+    options: [
+        '1Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        '2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        '3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        '4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+    ],
+};
 
-optionForm.addEventListener("submit", (e) => {
+const changeQuestions = (qOption) => {
+    questionContentP.textContent = qOption.question;
+    optionContentSpans.forEach((span, index) => {
+        span.textContent = qOption.options[index];
+    });
+};
+
+optionForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let options = Object.fromEntries(new FormData(optionForm));
     console.log(options);
-})
+    changeQuestions(qOption);
+});
