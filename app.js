d6b88234d1c04dd192da0a5f446656c0591c09da
Pikarestiv <okwyodumodu@gmail.com>
2020-06-23 13:00:35 +0100

    separate questions into a different file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import {qOptions} from './questions.js';
 const nextButton = document.querySelector('#next-button');
 const submitButton = document.querySelector('#submit-button');
 const optionForm = document.querySelector('#option-form');
@@ -6,36 +7,6 @@ const optionInputs = document.querySelectorAll('.option input');
 const optionContentSpans = document.querySelectorAll('.option-content');
 let currentQuestion = 0;
 const answeredOptions = [];
-const qOptions = [
-    {
-        question: '2Jose Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
-        options: [
-            '1Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-            '2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-            '3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-            '4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-        ],
-    },
-    {
-        question: '3Jose Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
-        options: [
-            '1Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-            '2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-            '3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-            '4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-        ],
-    },
-    {
-        question: '4Jose Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
-        options: [
-            '1Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-            '2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-            '3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-            '4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
-        ],
-    },
-];
-
 const changeDOMQuestion = (qOption) => {
     questionContentP.textContent = qOption.question;
     optionContentSpans.forEach((span, index) => {
---
+++ questions.js
@@ -0,0 +1,29 @@
+export const qOptions = [
+  {
+    question: '2Jose Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+    options: [
+        '1Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        '2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        '3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        '4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+    ],
+  },
+  {
+    question: '3Jose Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+    options: [
+        '1Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        '2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        '3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        '4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+    ],
+  },
+  {
+    question: '4Jose Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+    options: [
+        '1Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        '2Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        '3Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+        '4Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp',
+    ],
+  },
+];
